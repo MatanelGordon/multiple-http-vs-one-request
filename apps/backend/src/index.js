0928cc6e4b0aa7ffe60dc6e1693e72d2b544e6ca
Matanel Mac <mmgordon.matanel@gmail.com>
2022-05-03 23:28:46 +0300

    adding packages + making basic server

--- apps/backend/src/index.js
+++ apps/backend/src/index.js
@@ -1,1 +1,14 @@
-console.log('or its workinr');
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const port = process.env.PORT ?? 8000;
+
+app.use(cors());
+
+app.get('/all', (req,res) => {
+    res.json({a:1,b:2});
+})
+app.listen(port, () => {
+    console.log(`running in port ${port}`);
+})
