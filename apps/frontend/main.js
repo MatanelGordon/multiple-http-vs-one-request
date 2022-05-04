8eb5b6d18d536a5e520436b1edfda77a2611365b
Matanel Mac <mmgordon.matanel@gmail.com>
2022-05-04 19:12:35 +0300

    making basic frontend components

--- apps/frontend/main.js
+++ apps/frontend/main.js
@@ -2,12 +2,10 @@ import './style.css'
 
 const serverRoute = 'http://localhost:8000';
 const url = (path) => new URL(path, serverRoute);
-const result = document.getElementById('fetch-result');
 
 //begining
 const fetchedResRaw = await fetch(url`/all`.href)
 
 const fetchedRes = await fetchedResRaw.json();
 
-result.textContent = JSON.stringify(fetchedRes);
 
