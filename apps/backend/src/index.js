aff57c69591d42a00acb07f9e111d997ba047db3
Matanel Mac <mmgordon.matanel@gmail.com>
2022-05-05 17:48:40 +0300

    finished this shit

--- apps/backend/src/index.js
+++ apps/backend/src/index.js
@@ -1,14 +1,47 @@
 const express = require('express');
 const cors = require('cors');
+const _ = require('lodash');
+const axios = require('axios');
+const bigRequest = require('./bigRequest.json');
+const smallRequest = require('./smallRequest.json');
 
 const app = express();
 const port = process.env.PORT ?? 8000;
 
+const getFetch = async (url) => {
+    const result = await axios.get(url);
+    return result.data;
+}
+
 app.use(cors());
 
-app.get('/all', (req,res) => {
-    res.json({a:1,b:2});
+const parts = 10;
+app.get('/all', async (req,res) => {
+    const fetches = _.chain(new Array(parts))
+    .map((_, i) => i + 1)
+    .map(id => `http://localhost:${port}/partial/${id}`)
+    .map(getFetch)
+    .value()
+
+    const results = await Promise.all(fetches);
+    res.json(results.flat());
 })
+
+app.get('/partial/:id', (req,res) => {
+    const size = Math.ceil(bigRequest.length / parts);
+    const {id} = req.params;
+    const obj = bigRequest.slice(size * (id - 1), size * id);
+    res.json(obj);
+})
+
+app.get('/parts', (_,res) => {
+    res.json({parts})
+})
+
+app.get('/small', (req,res) => {
+    res.json(smallRequest);
+})
+
 app.listen(port, () => {
     console.log(`running in port ${port}`);
 })
--- apps/frontend/main.js
+++ apps/frontend/main.js
@@ -3,9 +3,95 @@ import './style.css'
 const serverRoute = 'http://localhost:8000';
 const url = (path) => new URL(path, serverRoute);
 
-//begining
-const fetchedResRaw = await fetch(url`/all`.href)
+const app = document.getElementById('app');
 
-const fetchedRes = await fetchedResRaw.json();
+const sleep = ms => new Promise(res => setTimeout(res, ms));
+
+const createCard = (container = app, id = null) => {
+    const node = document.createElement('div');
+    node.classList.add(
+        'pending', 'border-2', 'py-3','px-5', 
+        'font-bold', 'border-cardColor', 'text-cardColor',
+        'text-center', 'rounded-lg', 'w-80'
+    )
+
+    node.textContent = 'Pending...'
+
+    if(id){
+        node.setAttribute('id', id);
+    }
+    
+    container.appendChild(node);
+
+    return {
+        node,
+        successCard: () => {
+            node.classList.remove('pending');
+            node.classList.add('success');
+            node.textContent = 'Success!'
+        },
+        setTime: time => {
+            node.classList.remove('pending');
+            node.classList.add('time');
+            node.textContent = `${+time} [ms]`
+        }
+    }
+}
+
+const createDivider = (container = app) => {
+    const divider = document.createElement('i');
+    divider.classList.add('w-1/2', 'h-0.5', 'bg-gray-500', 'my-5')
+    if(container){
+        container.appendChild(divider);
+    }
+    return divider;
+}
+
+const createTitle = (content, container = app, element = 'h2') => {
+    const header = document.createElement(element);
+    header.textContent = content;
+    container.appendChild(header);
+    return header;
+}
+
+//begining
+
+//multiple in server
+createTitle('multiple fetches in server')
+const card = createCard();
+
+await sleep(1000)
+
+const startTime1 = new Date();
+await fetch(url`/all`.href).then(card.successCard);
+const time1 = new Date(new Date() - startTime1);
+createCard().setTime(time1)
+
+//divider
+const divider = createDivider();
+
+//multiple in client
+const partsFetch = await fetch(url`/parts`)
+const {parts} = await partsFetch.json()
+
+createTitle(`multiple (${parts}) fetches in client`)
+
+const cards = new Array(parts)
+    .fill(0)
+    .map(() => createCard());
+
+await sleep(1000);
+const startTime2 = new Date();
+const promises = new Array(parts)
+    .fill(0)
+    .map((_, i) => i + 1)
+    .map(id => url`/partial/${id}`.href)
+    .map((addr, i) => fetch(addr).then(cards[i].successCard))
+
+await Promise.all(promises);
+const diff2 = new Date(new Date() - startTime2);
+createCard().setTime(diff2);
+
+
 
 
