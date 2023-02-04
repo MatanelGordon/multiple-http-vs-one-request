0dd3b490835ba151bb6a4b3f89e14c1f0ed5f20c
Matanel Mac <mmgordon.matanel@gmail.com>
2023-02-04 02:18:59 +0200

    renamed variables and added env vars

--- apps/backend/src/index.js
+++ apps/backend/src/index.js
@@ -3,12 +3,13 @@ const cors = require("cors");
 const nocache = require('nocache');
 const axios = require("axios");
 const createRequestBody = require('./createRequest');
-const parts = 30;
-const chunkSize = 5;
-const bigRequest = createRequestBody(parts * chunkSize);
+
+const PARTS = process.env['PARTS'] ?? 30;
+const CHUNK_SIZE = process.env['CHUNK_SIZE'] ?? 5;
+const bigRequest = createRequestBody(PARTS * CHUNK_SIZE);
 
 const app = express();
-const port = process.env.PORT ?? 8000;
+const port = process.env['VITE_PORT'] ?? 8000;
 
 const getFetch = async (url) => {
   const result = await axios.get(url);
@@ -18,7 +19,7 @@ const getFetch = async (url) => {
 app.use(cors());
 app.use(nocache());
 
-app.use((req, res, next) => {
+app.use((req, _, next) => {
   console.log(`incoming: ${req.url}`);
   next();
 });
@@ -26,7 +27,7 @@ app.use((req, res, next) => {
 app.set('Etag', false);
 
 app.get("/all", async (req, res) => {
-  const fetches = new Array(parts).fill(0).map((_, i) => {
+  const fetches = new Array(PARTS).fill(0).map((_, i) => {
     const id = i + 1;
     const addr = `http://localhost:${port}/partial/${id}`;
     return getFetch(addr);
@@ -37,7 +38,7 @@ app.get("/all", async (req, res) => {
 });
 
 app.get("/partial/:id", (req, res) => {
-  const size = Math.ceil(bigRequest.length / parts);
+  const size = Math.ceil(bigRequest.length / PARTS);
   const { id } = req.params;
   if(!parseInt(id)) { res.status(400).json({message: `could not find id ${id}`, input: id}) }
   const obj = bigRequest.slice(size * (id - 1), size * id);
@@ -45,7 +46,7 @@ app.get("/partial/:id", (req, res) => {
 });
 
 app.get("/settings", (_, res) => {
-  res.json({ parts, chunkSize });
+  res.json({ PARTS, CHUNK_SIZE });
 });
 
 app.listen(port, () => {
